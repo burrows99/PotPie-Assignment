014545911fac08242b0951fa5a64d467847308fd
burrows99 <rburrows@sciometrix.com>
2025-05-07 18:29:11 +0100

    DB connected to fast API app

--- backend/dependencies.py
+++
@@ -1,7 +0,0 @@
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from db.config import SessionLocal
-
-async def get_db() -> AsyncSession:
-    async with SessionLocal() as session:
-        yield session
